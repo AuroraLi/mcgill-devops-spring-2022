135588fc6ceb001c203aab3958f1e68f98157e1d
Your Name <you@example.com>
2022-02-19 15:38:54 +0000

    try

--- lab-1-terraform-resources/terraform/resource-group/main.tf
+++ lab-1-terraform-resources/terraform/resource-group/main.tf
@@ -2,9 +2,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-devops-tf-${random_uuid.uuid.result}"
   location = var.location
 
-  tags = {
-    environment = "dev"
-  }
 }
 
 resource "random_uuid" "uuid" {}
