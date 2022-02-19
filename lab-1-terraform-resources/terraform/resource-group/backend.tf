8969f6ac2f9a45c34763845baf64c0b10d29dea5
Your Name <you@example.com>
2022-02-19 15:31:47 +0000

    Adding Environment Tag for Compliance Checks

--- lab-1-terraform-resources/terraform/resource-group/backend.tf
+++ lab-1-terraform-resources/terraform/resource-group/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    features {}
     resource_group_name  = "github-actions-tfstate"
     storage_account_name = "githubliauroratfstate"
     container_name       = "tfstatedevops"
