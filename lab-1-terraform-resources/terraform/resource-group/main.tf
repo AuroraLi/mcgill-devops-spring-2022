2db92dccd645cc88c06fbdeeda92b639e7f3c6f7
Your Name <you@example.com>
2022-02-19 15:24:11 +0000

    Adding Environment Tag for Compliance Checks

--- lab-1-terraform-resources/terraform/resource-group/main.tf
+++ lab-1-terraform-resources/terraform/resource-group/main.tf
@@ -9,9 +9,4 @@ resource "azurerm_resource_group" "rg" {
 
 resource "random_uuid" "uuid" {}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-devops-tf-${random_uuid.uuid.result}"
-  location = var.location
-
-
 }
